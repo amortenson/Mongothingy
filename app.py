94b66bf6d4e33974608f8936b9df7e677bfb5896
trunkatedpig <aamortenson1@gmail.com>
2014-10-31 12:22:32 -0400

    stuff done kind of

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 import random
 from flask import Flask,request,redirect,render_template,session
 from pymongo import Connection
+
 app=Flask(__name__)
-
-
-
 
 @app.route("/", methods=["GET","POST"])
 @app.route("/home", methods=["GET","POST"])
-def index():
+def home():
     return render_template("main.html");
 
 @app.route("/login", methods=["GET","POST"])
-def forums():
+def login():
     return render_template("login.html", topics = topics)
 
 @app.route("/page1", methods=["GET","POST"])
-def create():
-    return render_template("page1.html")
+def page1():
+    return render_template("page1.html", loggedin=loggedin, username = username)
 
 @app.route("/page2", methods=["GET","POST"])
-def table():
+def page2():
     return render_template("page2.html");
 
      
