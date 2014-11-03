316dadd786b4629f3908b14361c5c411a56fc156
CallMeSenpai <ericliao10797@hotmail.com>
2014-11-02 21:35:18 -0500

    iforgotwhatidid

--- app.py
+++ app.py
@@ -13,12 +13,21 @@ def home():
 def login():
     return render_template("login.html")
 
+@app.route("/register",methods=["GET","POST"])
+def register():
+    return render_template("register.html")
+
 @app.route("/page1", methods=["GET","POST"])
 def page1():
-    if method == "GET":
+    ##universal dict. combine get and post
+    ud = dict(request.form.items() + request.args.items())
+    
+    if "username" not in ud or method == "GET":
         return render_template("page1.html", loggedin=False,username="")
     else:
-        #things
+        return render_template("page1.html",
+                               loggedin=True,
+                               username=ud["username"])
 
 @app.route("/page2", methods=["GET","POST"])
 def page2():
