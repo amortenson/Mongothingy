e3959f2d94ab7c3a4258f3ecd224be60ecf35341
Aaron Mortenson <aamortenson1@gmail.com>
2014-11-16 22:14:47 -0500

    decorated things a bit for the holidays

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import random
 from flask import Flask,request,redirect,render_template,session,make_response
 from pymongo import Connection
+from functools import wraps
 
 conn = Connection()
 db = conn["swaggy_database"]
@@ -8,39 +8,35 @@ users = db["users"]
 
 app=Flask(__name__)
 
-def check_login():
-    def validate():
-        if "username" in session and "password" in session:
-            if db.users.find({"username": session["username"],"password":session["password"]}).count()==1:
-                return render_template("main.html", loggedin=True,username=username)
-            else:
-                return render_template("main.html")
-    return validate()
 
+def loggedin():
+    return "username" in session and "password" in session and db.users.find({"username": session["username"],"password":session["password"]}).count()==1
 
+def check_login(f):
+    @wraps(f)
+    def inner(*args, **kwargs):
+        if loggedin():
+            return f(*args, **kwargs)
+        else:
+            return render_template("page1.html", loggedin=False,username="")
+    return inner
 
 @app.route("/", methods=["GET","POST"])
 @app.route("/home", methods=["GET","POST"])
-@check_login
-def home():
-    ## return stuff like check_login idk
-    return check_login()
-
+def home():    
+    if loggedin():
+        return render_template("main.html", loggedin=True,username=session["username"])
+    else:
+        return render_template("main.html")
 
 @app.route("/login", methods=["GET","POST"])
 def login():
-
     ud = dict(request.form.items() + request.args.items())
     if request.method == "GET":
-        username = ""
-        password = ""
-        if "username" in session and "password" in session:
-            username = session["username"]
-            password = session["password"]
-            if db.users.find({"username": username,"password":password}).count()==1:
-                return render_template("login.html", loggedin=True,username=username)
-            else:
-                return render_template("login.html")
+        if loggedin():
+            return render_template("login.html", loggedin=True,username=session["username"])
+        else:
+            return render_template("login.html")
     else:
         if db.users.find({"username": ud["username"],"password":ud["password"]}).count()==1:
             session["username"] = ud["username"]
@@ -53,72 +49,49 @@ def login():
 def register():
     username = ""
     password = ""
-    loggedin = False
     if "username" in session and "password" in session:
         username = session["username"]
         password = session["password"]
-        loggedin =  db.users.find({"username": username,"password":password}).count()==1
 
     ud = dict(request.form.items() + request.args.items())
     if request.method=="POST": ##already filled
         if ud["username"]!="" and ud["password"]!="" and ud["password2"]!="":
             if  False: ## username already in database
                 ##user taken. red text
-                return render_template("register.html",userTaken=True, username = username, loggedin = loggedin)
+                return render_template("register.html",userTaken=True, username = username, loggedin = loggedin())
             elif ud["password"] != ud["password2"]:
                 ##different passwords. red text
-                return render_template("register.html",diffPass=True, username = username, loggedin = loggedin)
+                return render_template("register.html",diffPass=True, username = username, loggedin = loggedin())
             else:
                 ##do db stuff
                 newUser = {"username":ud["username"],
                            "password":ud["password"]}
                 users.insert(newUser)
                 print "lol new user"
-                return render_template("redirect.html",target="home",registered=True, username = username, loggedin = loggedin) 
+                return render_template("redirect.html",target="home",registered=True, username = username, loggedin = loggedin()) 
         else:
             ##not complete. red text.
-            return render_template("register.html",notComplete=True, username = username, loggedin = loggedin)
+            return render_template("register.html",notComplete=True, username = username, loggedin = loggedin())
             
 
     else:
-        return render_template("register.html",samePass=True, username = username, loggedin = loggedin)
-        
+        return render_template("register.html",samePass=True, username = username, loggedin = loggedin())
+      
 @app.route("/page1", methods=["GET","POST"])
-def page1():
-    username = ""
-    password = ""
-    if "username" in session and "password" in session:
-        username = session["username"]
-        password = session["password"]
-    ##universal dict. combine get and post
-    ud = dict(request.form.items() + request.args.items())
-    if db.users.find({"username": username,"password":password}).count()==1:
-        return render_template("page1.html",
-                               loggedin=True,
-                               username=username)
-    else:
-        return render_template("page1.html", loggedin=False,username="")
-   
-        
+@check_login 
+def page1(): 
+    return render_template("page1.html",loggedin=True,username=session["username"])
 
 @app.route("/page2", methods=["GET","POST"])
+@check_login
 def page2():
     ud = dict(request.form.items() + request.args.items())
     if "logout" in ud:
         session["username"] = ""
         session["password"] = ""
+        return render_template("page2.html",loggedin=False)
 
-    username = ""
-    password = ""
-    loggedin = False
-    if "username" in session and "password" in session:
-        username = session["username"]
-        password = session["password"]
-    if db.users.find({"username": username,"password":password}).count()==1:
-        loggedin=True
-    
-
-    return render_template("page2.html", loggedin=loggedin,username=username)
+    return render_template("page2.html", loggedin=True,username=session["username"])
 
  
 if __name__=="__main__":
