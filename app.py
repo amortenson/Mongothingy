3bc57fa9b938865e5615295cd66c0290cd130965
Aaron Mortenson <aamortenson1@gmail.com>
2015-03-31 09:57:50 -0400

    stuff works

--- app.py
+++ app.py
@@ -1,100 +1,63 @@
 from flask import Flask,request,redirect,render_template,session,make_response
-from pymongo import Connection
+from pymongo import MongoClient
 from functools import wraps
+import json
 
-conn = Connection()
-db = conn["swaggy_database"]
-users = db["users"]
+mongo = MongoClient()
+db = mongo["teh_illest_database"]
 
 app=Flask(__name__)
 
+def read():
+    cursor = collection.find()
+    return toJSON(cursor)
 
-def loggedin():
-    return "username" in session and "password" in session and db.users.find({"username": session["username"],"password":session["password"]}).count()==1
+def create(username, itemname):
+    data = {
+        'username': username,
+        'itemname': itemname
+    }
+    return json.dumps({'_id': str(collection.insert(data))})
 
-def check_login(f):
-    @wraps(f)
-    def inner(*args, **kwargs):
-        if loggedin():
-            return f(*args, **kwargs)
-        else:
-            return render_template("page1.html", loggedin=False,username="")
-    return inner
+@app.route("/delete/<userName>/<itemName>", methods=["GET","POST","DELETE"])
+def delete(userName,itemName):
+    print "tryna delete stuff here"
+    db.places.remove( { 'username': userName, 'itemname': itemName } );
+    places = [x for x in db.places.find()]
+    print json.dumps(places)
+    return json.dumps(places)
 
 @app.route("/", methods=["GET","POST"])
-@app.route("/home", methods=["GET","POST"])
 def home():    
-    if loggedin():
-        return render_template("main.html", loggedin=True,username=session["username"])
-    else:
-        return render_template("main.html")
+    return render_template("index.html")
 
-@app.route("/login", methods=["GET","POST"])
-def login():
-    ud = dict(request.form.items() + request.args.items())
-    if request.method == "GET":
-        if loggedin():
-            return render_template("login.html", loggedin=True,username=session["username"])
-        else:
-            return render_template("login.html")
-    else:
-        if db.users.find({"username": ud["username"],"password":ud["password"]}).count()==1:
-            session["username"] = ud["username"]
-            session["password"] = ud["password"]
-            return render_template("page1.html",loggedin = True, username = ud["username"])
-        else:
-            return render_template("login.html", loggedin = False, error = True)
+@app.route("/places")
+def places():
+    places = [x  for x in db.places.find()]
+    print json.dumps(places)
+    return json.dumps(places)
 
-@app.route("/register",methods=["GET","POST"])
-def register():
-    username = ""
-    password = ""
-    if "username" in session and "password" in session:
-        username = session["username"]
-        password = session["password"]
+@app.route("/place",methods=['GET','POST','DELETE','PUT'])
+@app.route("/place/<id>",methods=['GET','POST','DELETE','PUT'])
+def place(id=None):
+    method = request.method
+    j = request.get_json();
+    print method, id, j
+    if id ==None:
+        id =j['time'] #allows two posts with same username
+        
+    if method == "POST":
+        print "HEY BUDDY WE'RE POSTING"
+        j['_id']=id
+        try:
+            x = db.places.update({'_id':id},j,upsert=True)
+        except:
+            j.pop("_id",None)
+            x = db.places.update({'_id':id},j)    
 
-    ud = dict(request.form.items() + request.args.items())
-    if request.method=="POST": ##already filled
-        if ud["username"]!="" and ud["password"]!="" and ud["password2"]!="":
-            if  False: ## username already in database
-                ##user taken. red text
-                return render_template("register.html",userTaken=True, username = username, loggedin = loggedin())
-            elif ud["password"] != ud["password2"]:
-                ##different passwords. red text
-                return render_template("register.html",diffPass=True, username = username, loggedin = loggedin())
-            else:
-                ##do db stuff
-                newUser = {"username":ud["username"],
-                           "password":ud["password"]}
-                users.insert(newUser)
-                print "lol new user"
-                return render_template("redirect.html",target="home",registered=True, username = username, loggedin = loggedin()) 
-        else:
-            ##not complete. red text.
-            return render_template("register.html",notComplete=True, username = username, loggedin = loggedin())
-            
-
-    else:
-        return render_template("register.html",samePass=True, username = username, loggedin = loggedin())
-      
-@app.route("/page1", methods=["GET","POST"])
-@check_login 
-def page1(): 
-    return render_template("page1.html",loggedin=True,username=session["username"])
-
-@app.route("/page2", methods=["GET","POST"])
-@check_login
-def page2():
-    ud = dict(request.form.items() + request.args.items())
-    if "logout" in ud:
-        session["username"] = ""
-        session["password"] = ""
-        return render_template("page2.html",loggedin=False)
-
-    return render_template("page2.html", loggedin=True,username=session["username"])
-
+    return json.dumps({'result':x})
  
 if __name__=="__main__":
-    app.secret_key="b0kun0p1c0c7f"
+    #app.secret_key="b0kun0p1c0c7f"
     app.debug=True
     app.run();
